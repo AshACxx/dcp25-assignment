244ffdad4f01e85d6eff84a10f6777788fa74132
AshACxx <irishjatt01@gmail.com>
2025-12-02 21:42:44 +0000

    created my buttons for the user input

--- starter_code.py
+++ starter_code.py
@@ -1,11 +1,3 @@
-#IMPORTANT, I HAD TO DELETE MY REPO AND RETSART EVERYTHING, LOTS OF PROGRESS MAY BE MADE IN A SHORT TIME
-
-# Starter code for Data Centric Programming Assignment 2025
-
-# os is a module that lets us access the file system
-
-# Bryan Duggan likes Star Trek
-# Bryan Duggan is a great flute player
 
 import os 
 import sqlite3
@@ -13,41 +5,6 @@ import pandas as pd
 import tkinter as tk
 from tkinter import scrolledtext
 
-# sqlite for connecting to sqlite databases
-
-# An example of how to create a table, insert data
-# and run a select query
-'''
-def do_databasse_stuff():
-
-    conn = sqlite3.connect('tunes.db')
-    cursor = conn.cursor()
-
-    # Create table
-    cursor.execute('CREATE TABLE IF NOT EXISTS users (name TEXT, age INTEGER)')
-
-    # Insert data
-    cursor.execute('INSERT INTO users (name, age) VALUES (?, ?)', ('John', 30))
-
-    # Save changes
-    conn.commit()
-
-    cursor.execute('SELECT * FROM users')
-
-    # Get all results
-    results = cursor.fetchall()
-
-    # Print results
-    for row in results:
-        print(row)    
-        print(row[0])
-        print(row[1])
-    # Close
-    
-    df = pd.read_sql("SELECT * FROM users", conn)
-    print(df.head())
-    conn.close()
-'''
 def my_sql_database():
     ''' 
     old code 
@@ -108,10 +65,14 @@ def process_file(file):
         elif line.startswith("R:"):
             current_tune["type"] = line[2:].strip()
             
-        #current_tune["body"] taking "body from the dictionary and adding the lines that dont have x t or k
+        #current_tune["body"] taking "body from the dictionary and adding the lines that dont have x t k or r
         else:
+            
+            
             if "body" not in current_tune:
+                #making body a key
                 current_tune["body"] = ""
+                #adding on body to a new line
             current_tune["body"] += line + "\n"
                 
                 
@@ -191,18 +152,23 @@ def get_tune(df, tune_type):
     df_2 = df[df["type"]==tune_type]
     return df_2
 
+#printing the title and key
 book2_t = get_tunes_by_book(df, 2)
 print(book2_t[["title","key"]].head())
 
+
+#function to return the title of a song
 def search(df, search_terms):
     df_3 = df[df["title"].str.contains(search_terms, case = False)]
     return df_3
+#returning rthe title andd jig of a song
 
 book3_t = get_tune(df, "Single jig")
 print(book3_t[["title","type"]].head())
 
 #tkinter menu
 def launch_gui(df):
+    
     #main window
     my_w = tk.Tk()
     my_w.title("ABC Tune Database")
@@ -234,3 +200,36 @@ def launch_gui(df):
             #adding lines to seperate 
             output.insert(tk.END, "-"*50 + "\n")
     
+
+    #search by book number
+    #.pack is used to horiztonally or vertically place the widgets after another 
+    #.pack() lets me stack the name and asearch bar on top of eachother
+    
+    tk.Label (my_w, text="Search by Book Number:", font=("Arial",10)).pack()
+    
+    book_entry = tk.Entry(my_w)
+    book_entry.pack()
+    
+    #creating a button, using lambda to create a mini function name (tkinter cant take calls) inserting the dataframe and using .get() to return the specified key
+    #FOR FUTURE: user types into entry > when u click the button mini funcyion runs > returns number > get_tunes_by_book filters dataframe to book (1)or(2)
+    
+    tk.Button(my_w, text="Search", command=lambda:show(get_tunes_by_book(df, int(book_entry.get())))).pack()
+
+    #search by tune type
+    tk.Label(my_w, text="\nSearch by Type:").pack()
+    type_entry = tk.Entry(my_w)
+    
+    type_entry.pack()
+    tk.Button(my_w, text="Search", command=lambda: show(get_tune(df, type_entry.get()))).pack()
+
+    # Search by title
+    tk.Label(my_w, text="\nSearch by Title:").pack()
+    title_entry = tk.Entry(my_w)
+    title_entry.pack()
+    tk.Button(my_w, text="Search", command=lambda: show(search(df, title_entry.get()))).pack()
+
+    my_w.mainloop()
+
+
+# REPLACE TEXT UI WITH TKINTER
+launch_gui(df)
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,8 +1,2 @@
-def my_sql_database():
-    conn = sqlite3.connect("tunes2.db")
 
-    cursor = conn.cursor()
-
-    cursor.execute("DELETE FROM tunes2")
-    conn.commit()
-    conn.close()
+#tkinter menu
