d3fb00df5ce9c3f45e6ee826add9791c9e1b04f8
AshACxx <irishjatt01@gmail.com>
2025-12-05 00:48:46 +0000

    added photos

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,5 @@
 
+def search(df, search_terms):
+    '''function to return the title of a song (used for tkinter)'''
+    df_3 = df[df["title"].str.contains(search_terms, case = False)]
+    return df_3
